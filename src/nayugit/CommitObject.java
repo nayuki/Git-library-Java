67660765f324f5d29902786227c36c423e9ce7f7
Nayuki Minase <nayuki@eigenstate.org>
2014-10-23 02:55:09 +0000

    CommitObject: Added class, modified Repository to use it.

---
+++ src/nayugit/CommitObject.java
@@ -0,0 +1,98 @@
+package nayugit;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.DataFormatException;
+
+
+public final class CommitObject extends GitObject {
+	
+	public final ObjectId tree;
+	public List<ObjectId> parents;
+	public final String message;
+	
+	public final String authorName;
+	public final String authorEmail;
+	public final int authorTime;
+	public final int authorTimezone;
+	
+	public final String committerName;
+	public final String committerEmail;
+	public final int committerTime;
+	public final int committerTimezone;
+	
+	
+	
+	
+	public CommitObject(ObjectId id, byte[] data) throws UnsupportedEncodingException, DataFormatException {
+		super(id);
+		parents = new ArrayList<ObjectId>();
+		
+		int index = 0;
+		int start;
+		String line;
+		String[] parts;
+		
+		for (start = index; data[index] != '\n'; index++);
+		line = new String(data, start, index - start, "UTF-8");
+		parts = line.split(" ", 2);
+		if (!parts[0].equals("tree"))
+			throw new DataFormatException("Tree field expected");
+		tree = new ObjectId(parts[1]);
+		index++;
+		
+		while (true) {
+			for (start = index; data[index] != '\n'; index++);
+			line = new String(data, start, index - start, "UTF-8");
+			parts = line.split(" ", 2);
+			if (!parts[0].equals("parent"))
+				break;
+			parents.add(new ObjectId(parts[1]));
+			index++;
+		}
+		
+		if (!parts[0].equals("author"))
+			throw new DataFormatException("Author field expected");
+		Matcher m = AUTHORSHIP_PATTERN.matcher(parts[1]);
+		if (!m.matches())
+			throw new DataFormatException("Invalid author data");
+		authorName = m.group(1);
+		authorEmail = m.group(2);
+		authorTime = Integer.parseInt(m.group(3));
+		authorTimezone = Integer.parseInt(m.group(4) + "1") * (Integer.parseInt(m.group(5)) * 60 + Integer.parseInt(m.group(6)));
+		index++;
+		
+		for (start = index; data[index] != '\n'; index++);
+		line = new String(data, start, index - start, "UTF-8");
+		parts = line.split(" ", 2);
+		if (!parts[0].equals("committer"))
+			throw new DataFormatException("Committer field expected");
+		m = AUTHORSHIP_PATTERN.matcher(parts[1]);
+		if (!m.matches())
+			throw new DataFormatException("Invalid committer data");
+		committerName = m.group(1);
+		committerEmail = m.group(2);
+		committerTime = Integer.parseInt(m.group(3));
+		committerTimezone = Integer.parseInt(m.group(4) + "1") * (Integer.parseInt(m.group(5)) * 60 + Integer.parseInt(m.group(6)));
+		index++;
+		
+		if (data[index] != '\n')
+			throw new DataFormatException("Blank line expected");
+		index++;
+		message = new String(data, index, data.length - index, "UTF-8");
+	}
+	
+	
+	
+	public String toString() {
+		return String.format("CommitObject(id=%s, tree=%s)", id.hexString, tree.hexString);
+	}
+	
+	
+	
+	private static final Pattern AUTHORSHIP_PATTERN = Pattern.compile("(.*?) <([^>]*)> (\\d+) ([+-])(\\d{2})(\\d{2})");
+	
+}
--- src/nayugit/Repository.java
+++ src/nayugit/Repository.java
@@ -3,8 +3,8 @@ package nayugit;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.zip.DataFormatException;
 import java.util.zip.InflaterInputStream;
@@ -29,24 +29,56 @@ public final class Repository {
 	}
 	
 	
-	public byte[] readRawObject(ObjectId id) throws IOException {
+	public byte[] readRawObject(ObjectId id) throws IOException, DataFormatException {
 		File file = new File(directory, "objects" + File.separator + id.hexString.substring(0, 2) + File.separator + id.hexString.substring(2));
-		if (!file.isFile())
-			throw new FileNotFoundException();
-		InflaterInputStream in = new InflaterInputStream(new FileInputStream(file));
-		try {
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			byte[] buf = new byte[1024];
-			while (true) {
-				int n = in.read(buf);
-				if (n == -1)
-					break;
-				out.write(buf, 0, n);
+		if (file.isFile()) {
+			// Read from loose object store
+			InputStream in = new InflaterInputStream(new FileInputStream(file));
+			try {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				byte[] buf = new byte[1024];
+				while (true) {
+					int n = in.read(buf);
+					if (n == -1)
+						break;
+					out.write(buf, 0, n);
+				}
+				return out.toByteArray();
+			} finally {
+				in.close();
 			}
-			return out.toByteArray();
-		} finally {
-			in.close();
+			
+		} else {
+			// Scan pack files
+			for (File item : new File(directory, "objects" + File.separator + "pack").listFiles()) {
+				String name = item.getName();
+				if (item.isFile() && name.startsWith("pack-") && name.endsWith(".idx")) {
+					File packFile = new File(item.getParentFile(), name.substring(0, name.length() - 3) + "pack");
+					Object[] temp = new PackfileReader(item, packFile).readRawObject(id);
+					if (temp != null) {
+						int type = (Integer)temp[0];
+						byte[] data = (byte[])temp[1];
+						String typeStr;
+						if (type == 1)
+							typeStr = "commit";
+						else if (type == 2)
+							typeStr = "tree";
+						else if (type == 3)
+							typeStr = "blob";
+						else
+							throw new DataFormatException("Unknown object type: " + type);
+						byte[] header = (typeStr + " " + data.length + "\0").getBytes("US-ASCII");
+						byte[] result = new byte[header.length + data.length];
+						System.arraycopy(header, 0, result, 0, header.length);
+						System.arraycopy(data, 0, result, header.length, data.length);
+						return result;
+					}
+				}
+			}
 		}
+		
+		// Object not found
+		return null;
 	}
 	
 	
@@ -75,6 +107,8 @@ public final class Repository {
 			return new BlobObject(id, bytes);
 		if (type.equals("tree"))
 			return new TreeObject(id, bytes);
+		if (type.equals("commit"))
+			return new CommitObject(id, bytes);
 		else
 			throw new DataFormatException("Unknown object type: " + type);
 	}
