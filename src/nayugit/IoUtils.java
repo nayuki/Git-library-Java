b2bec93932200e80d7d55fd11deda2e103293155
Nayuki Minase <nayuki@eigenstate.org>
2014-10-23 14:50:49 +0000

    IoUtils: Added class, moved methods from PackfileReader.

---
+++ src/nayugit/IoUtils.java
@@ -0,0 +1,37 @@
+package nayugit;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+
+final class IoUtils {
+	
+	public static int readUnsignedNoEof(InputStream in) throws IOException {
+		int b = in.read();
+		if (b == -1)
+			throw new EOFException();
+		return b & 0xFF;
+	}
+	
+	
+	public static int readInt32(RandomAccessFile raf) throws IOException {
+		byte[] b = new byte[4];
+		raf.readFully(b);
+		return b[0] << 24 | (b[1] & 0xFF) << 16 | (b[2] & 0xFF) << 8 | (b[3] & 0xFF);
+	}
+	
+	
+	public static void skipFully(InputStream in, int skip) throws IOException {
+		if (skip < 0)
+			throw new IllegalArgumentException();
+		while (skip > 0) {
+			long n = in.skip(skip);
+			if (n <= 0)
+				throw new EOFException();
+			skip -= n;
+		}
+	}
+	
+}
--- src/nayugit/PackfileReader.java
+++ src/nayugit/PackfileReader.java
@@ -36,28 +36,24 @@ public final class PackfileReader {
 			byte[] b;
 			
 			// Check header
-			b = new byte[8];
+			b = new byte[4];
 			indexRaf.readFully(b);
 			if (b[0] != -1 || b[1] != 't' || b[2] != 'O' || b[3] != 'c')
 				throw new DataFormatException("Pack index header expected");
-			if (toInt32(b, 4) != 2)
+			if (IoUtils.readInt32(indexRaf) != 2)
 				throw new DataFormatException("Index version 2 expected");
 			
 			// Read pack size
 			if (indexRaf.skipBytes(255 * 4) != 255 * 4)
 				throw new EOFException();
-			b = new byte[4];
-			indexRaf.readFully(b);
-			int totalObjects = toInt32(b, 0);
+			int totalObjects = IoUtils.readInt32(indexRaf);
 			
 			// Find object ID in index
 			int headByte = id.getByte(0) & 0xFF;
 			int objectOffset = 0;
 			if (headByte > 0) {
 				indexRaf.seek(8 + (headByte - 1) * 4);
-				b = new byte[4];
-				indexRaf.readFully(b);
-				objectOffset = toInt32(b, 0);
+				objectOffset = IoUtils.readInt32(indexRaf);
 			}
 			indexRaf.seek(8 + 256 * 4 + objectOffset * 20);
 			b = new byte[20];
@@ -71,10 +67,8 @@ public final class PackfileReader {
 					return null;
 				objectOffset++;
 			}
-			b = new byte[4];
 			indexRaf.seek(8 + 256 * 4 + totalObjects * 20 + totalObjects * 4 + objectOffset * 4);
-			indexRaf.readFully(b);
-			byteOffset = toInt32(b, 0);
+			byteOffset = IoUtils.readInt32(indexRaf);
 		} finally {
 			indexRaf.close();
 		}
@@ -105,14 +99,7 @@ public final class PackfileReader {
 			throw new IllegalArgumentException();
 		InputStream in = new FileInputStream(packFile);
 		try {
-			// Skip fully
-			int toSkip = byteOffset;
-			while (toSkip > 0) {
-				long n = in.skip(toSkip);
-				if (n <= 0)
-					throw new EOFException();
-				toSkip -= n;
-			}
+			IoUtils.skipFully(in, byteOffset);
 			
 			// Read decompressed size
 			int typeAndSize = decodeTypeAndSize(in);
@@ -170,12 +157,12 @@ public final class PackfileReader {
 						int off = 0;
 						for (int i = 0; i < 4; i++) {
 							if (((op >>> i) & 1) != 0)
-								off |= readByteNoEof(deltaIn) << (i * 8);
+								off |= IoUtils.readUnsignedNoEof(deltaIn) << (i * 8);
 						}
 						int len = 0;
 						for (int i = 0; i < 3; i++) {
 							if (((op >>> (i + 4)) & 1) != 0)
-								len |= readByteNoEof(deltaIn) << (i * 8);
+								len |= IoUtils.readUnsignedNoEof(deltaIn) << (i * 8);
 						}
 						if (len == 0)
 							len = 0x10000;
@@ -195,14 +182,14 @@ public final class PackfileReader {
 	
 	
 	private static int decodeTypeAndSize(InputStream in) throws IOException, DataFormatException {
-		int b = readByteNoEof(in);
+		int b = IoUtils.readUnsignedNoEof(in);
 		int type = (b >>> 4) & 7;
 		long size = b & 0xF;
 		
 		for (int i = 0; ; i++) {
 			if (i >= 6)
 				throw new DataFormatException("Variable-length integer too long");
-			b = readByteNoEof(in);
+			b = IoUtils.readUnsignedNoEof(in);
 			size |= (b & 0x7FL) << (i * 7 + 4);
 			if ((b & 0x80) == 0)
 				break;
@@ -220,7 +207,7 @@ public final class PackfileReader {
 		for (int i = 0; ; i++) {
 			if (i >= 5)
 				throw new DataFormatException("Variable-length integer too long");
-			int b = readByteNoEof(in);
+			int b = IoUtils.readUnsignedNoEof(in);
 			result |= b & 0x7F;
 			if ((b & 0x80) == 0)
 				break;
@@ -239,7 +226,7 @@ public final class PackfileReader {
 		for (int i = 0; ; i++) {
 			if (i >= 6)
 				throw new DataFormatException("Variable-length integer too long");
-			int b = readByteNoEof(in);
+			int b = IoUtils.readUnsignedNoEof(in);
 			result |= (b & 0x7FL) << (i * 7);
 			if ((b & 0x80) == 0)
 				break;
@@ -248,19 +235,6 @@ public final class PackfileReader {
 		if ((int)result != result)
 			throw new DataFormatException("Variable-length integer too large");
 		return (int)result;
-	}
-	
-	
-	private static int readByteNoEof(InputStream in) throws IOException {
-		int b = in.read();
-		if (b == -1)
-			throw new EOFException();
-		return b & 0xFF;  // Convert to unsigned
-	}
-	
-	
-	private static int toInt32(byte[] b, int off) {
-		return b[off + 0] << 24 | (b[off + 1] & 0xFF) << 16 | (b[off + 2] & 0xFF) << 8 | (b[off + 3] & 0xFF);
 	}
 	
 }
