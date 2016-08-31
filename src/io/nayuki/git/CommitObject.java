13e9f3c7d907413bd758128f103f91d4112213b3
Project Nayuki <me@nayuki.io>
2016-08-31 23:01:10 +0000

    CommitObject: Revamped and simplified the parser constructor logic for clarity.

--- src/io/nayuki/git/CommitObject.java
+++ src/io/nayuki/git/CommitObject.java
@@ -112,69 +112,61 @@ public final class CommitObject extends GitObject {
 	 * @param data the serialized commit data to read
 	 * @param repo the repository to set for object IDs
 	 * @throws NullPointerException if the array is {@code null}
+	 * @throws DataFormatException if malformed data was encountered during reading
 	 */
 	public CommitObject(byte[] data, WeakReference<Repository> repo) throws DataFormatException {
 		this();
 		if (data == null)
 			throw new NullPointerException();
 		
-		int index = 0;
-		int start;
-		String line;
-		String[] parts;
-		
-		// Parse tree line
-		for (start = index; data[index] != '\n'; index++);
-		line = new String(data, start, index - start, StandardCharsets.UTF_8);
-		parts = line.split(" ", 2);
-		if (!parts[0].equals("tree"))
-			throw new DataFormatException("Tree field expected");
-		tree = new TreeId(parts[1], repo);
-		index++;
-		
-		// Parse parent lines (0 or more)
-		while (true) {
-			for (start = index; data[index] != '\n'; index++);
-			line = new String(data, start, index - start, StandardCharsets.UTF_8);
-			parts = line.split(" ", 2);
-			if (!parts[0].equals("parent"))
-				break;
-			parents.add(new CommitId(parts[1], repo));
-			index++;
+		try {
+			LineParser parser = new LineParser(data);
+			
+			// Parse tree line
+			String[] parts = parser.nextLineAsPair();
+			if (!parts[0].equals("tree"))
+				throw new DataFormatException("Tree field expected");
+			tree = new TreeId(parts[1], repo);
+			
+			// Parse parent lines (zero or more)
+			while (true) {
+				parts = parser.nextLineAsPair();
+				if (!parts[0].equals("parent"))
+					break;
+				parents.add(new CommitId(parts[1], repo));
+			}
+			
+			// Parse author line
+			if (!parts[0].equals("author"))
+				throw new DataFormatException("Author field expected");
+			Matcher m = AUTHORSHIP_PATTERN.matcher(parts[1]);
+			if (!m.matches())
+				throw new DataFormatException("Invalid author data");
+			authorName = m.group(1);
+			authorEmail = m.group(2);
+			authorTime = Integer.parseInt(m.group(3));
+			authorTimezone = Integer.parseInt(m.group(4) + "1") * (Integer.parseInt(m.group(5)) * 60 + Integer.parseInt(m.group(6)));
+			
+			// Parse committer line
+			parts = parser.nextLineAsPair();
+			if (!parts[0].equals("committer"))
+				throw new DataFormatException("Committer field expected");
+			m = AUTHORSHIP_PATTERN.matcher(parts[1]);
+			if (!m.matches())
+				throw new DataFormatException("Invalid committer data");
+			committerName = m.group(1);
+			committerEmail = m.group(2);
+			committerTime = Integer.parseInt(m.group(3));
+			committerTimezone = Integer.parseInt(m.group(4) + "1") * (Integer.parseInt(m.group(5)) * 60 + Integer.parseInt(m.group(6)));
+			
+			// Grab message
+			if (!parser.nextLine().equals(""))
+				throw new DataFormatException("Blank line expected");
+			message = parser.getRemainder();
+			
+		} catch (IllegalStateException e) {
+			throw new DataFormatException("Unexpected end of commit data");
 		}
-		
-		// Parse author line
-		if (!parts[0].equals("author"))
-			throw new DataFormatException("Author field expected");
-		Matcher m = AUTHORSHIP_PATTERN.matcher(parts[1]);
-		if (!m.matches())
-			throw new DataFormatException("Invalid author data");
-		authorName = m.group(1);
-		authorEmail = m.group(2);
-		authorTime = Integer.parseInt(m.group(3));
-		authorTimezone = Integer.parseInt(m.group(4) + "1") * (Integer.parseInt(m.group(5)) * 60 + Integer.parseInt(m.group(6)));
-		index++;
-		
-		// Parse committer line
-		for (start = index; data[index] != '\n'; index++);
-		line = new String(data, start, index - start, StandardCharsets.UTF_8);
-		parts = line.split(" ", 2);
-		if (!parts[0].equals("committer"))
-			throw new DataFormatException("Committer field expected");
-		m = AUTHORSHIP_PATTERN.matcher(parts[1]);
-		if (!m.matches())
-			throw new DataFormatException("Invalid committer data");
-		committerName = m.group(1);
-		committerEmail = m.group(2);
-		committerTime = Integer.parseInt(m.group(3));
-		committerTimezone = Integer.parseInt(m.group(4) + "1") * (Integer.parseInt(m.group(5)) * 60 + Integer.parseInt(m.group(6)));
-		index++;
-		
-		// Grab message
-		if (data[index] != '\n')
-			throw new DataFormatException("Blank line expected");
-		index++;
-		message = new String(data, index, data.length - index, StandardCharsets.UTF_8);
 	}
 	
 	
@@ -233,5 +225,46 @@ public final class CommitObject extends GitObject {
 	
 	// For example: John Smith <jsmith@example.com> 1234567890 +0000
 	private static final Pattern AUTHORSHIP_PATTERN = Pattern.compile("(.*?) <([^>]*)> (\\d+) ([+-])(\\d{2})(\\d{2})");
+	
+	
+	
+	/*---- Helper class for parser constructor ----*/
+	
+	private static final class LineParser {
+		
+		private String text;
+		private int startIndex;
+		
+		
+		public LineParser(byte[] data) {
+			text = new String(data, StandardCharsets.UTF_8);
+			startIndex = 0;
+		}
+		
+		
+		public String nextLine() {
+			int end = text.indexOf('\n', startIndex);
+			if (end == -1)
+				throw new IllegalStateException("Next line does not exist");
+			String result = text.substring(startIndex, end);
+			startIndex = end + 1;
+			return result;
+		}
+		
+		
+		public String[] nextLineAsPair() {
+			String temp = nextLine();
+			String[] result = temp.split(" ", 2);
+			if (result.length != 2)
+				throw new IllegalStateException("Next line does not have 2 parts");
+			return result;
+		}
+		
+		
+		public String getRemainder() {
+			return text.substring(startIndex);
+		}
+		
+	}
 	
 }
